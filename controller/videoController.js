ef51daec6978d4ab09912e73b3f062a1eb928435
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-10-13 21:53:43 +0900

    feat pug를 이용하여 템플릿 제작 - 파일생성

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,2 +1,7 @@
-export const home = (req,res) => res.send("home");
-export const search = (req,res) => res.send("search");
+export const home = (req,res) => res.render("home");
+export const search = (req,res) => res.render("search");
+export const video = (req,res) => res.render("video");
+export const upload = (req,res) => res.render("upload");
+export const video_detail = (req,res) => res.render("video_detail");
+export const edit_video = (req,res) => res.render("edit_video");
+export const delete_video = (req,res) => res.render("delete_video");
