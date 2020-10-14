09dad2bfc57c6afa6d7f2234a804f7e867d310f0
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-10-14 21:51:15 +0900

    style mixin사용해서 home화면 html작성

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:32433,
+        title:'Video Awsome',
+        description:'This is something I love',
+        views:567,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator:{
+            id:121212,
+            name:"JeanYoung",
+            email:"jeanfree1@naver.com"
+        }
+    },
+    {
+        id:43534,
+        title:'Video perfect',
+        description:'This is something I love',
+        views:24,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator:{
+            id:121212,
+            name:"JeanYoung",
+            email:"jeanfree1@naver.com"
+        }
+    },
+    {
+        id:676457,
+        title:'Video nice',
+        description:'This is something I love',
+        views:34634,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator:{
+            id:121212,
+            name:"JeanYoung",
+            email:"jeanfree1@naver.com"
+        }
+    },
+    {
+        id:245324,
+        title:'Video super',
+        description:'This is something I love',
+        views:1234,
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator:{
+            id:121212,
+            name:"JeanYoung",
+            email:"jeanfree1@naver.com"
+        }
+    }
+];
