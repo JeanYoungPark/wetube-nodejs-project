477327024485307d5684820b4cfbb81a1f2c6840
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-10-14 20:19:16 +0900

    feat search get form으로 값 받아오기

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # wetube-nodejs-project
 clone youtube
+
+## pages:
+
+- [ ] Home
+- [ ] join
+- [ ] login
+- [x] search
+- [ ] user detail
+- [ ] edit profile
+- [ ] change password
+- [ ] upload
+- [ ] video detail
+- [ ] edit video
--- controller/videoController.js
+++ controller/videoController.js
@@ -1,5 +1,8 @@
 export const home = (req,res) => res.render("home",{pageTitle:"Home"});
-export const search = (req,res) => res.render("search",{pageTitle:"Search"});
+export const search = (req,res) => {
+    const {query:{term:searchingBy}} = req;
+    res.render("search",{pageTitle:"Search",searchingBy:searchingBy});
+}
 export const video = (req,res) => res.render("video",{pageTitle:"Video"});
 export const upload = (req,res) => res.render("upload",{pageTitle:"Upload"});
 export const video_detail = (req,res) => res.render("video_detail",{pageTitle:"Video Detail"});
