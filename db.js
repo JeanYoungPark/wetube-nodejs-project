05cbe01703e1ecffdc5d5bb287e0f837d1938dc0
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-10-20 20:53:27 +0900

    feat mongodb 연동

--- db.js
+++ db.js
@@ -1,50 +1,13 @@
-export const videos = [
-    {
-        id:32433,
-        title:'Video Awsome',
-        description:'This is something I love',
-        views:567,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            id:121212,
-            name:"JeanYoung",
-            email:"jeanfree1@naver.com"
-        }
-    },
-    {
-        id:43534,
-        title:'Video perfect',
-        description:'This is something I love',
-        views:24,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            id:121212,
-            name:"JeanYoung",
-            email:"jeanfree1@naver.com"
-        }
-    },
-    {
-        id:676457,
-        title:'Video nice',
-        description:'This is something I love',
-        views:34634,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            id:121212,
-            name:"JeanYoung",
-            email:"jeanfree1@naver.com"
-        }
-    },
-    {
-        id:245324,
-        title:'Video super',
-        description:'This is something I love',
-        views:1234,
-        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator:{
-            id:121212,
-            name:"JeanYoung",
-            email:"jeanfree1@naver.com"
-        }
-    }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/wetube", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+const handleOpen = () => console.log("Connected to DB");
+const handleError = (error) => console.log(`Error on DB Connection:${error}`);
+
+db.once("open",handleOpen);
+db.on("error",handleError);
--- init.js
+++ init.js
@@ -1,4 +1,6 @@
+import "./db";
 import app from "./app";
+
 const PORT = 4000;
 const handleListening = () =>  console.log(`Listening on : http://localhost:${PORT}`);
 
