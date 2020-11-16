57c0233aff07093666652a8c87c671cbbb243f7f
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-11-16 18:52:50 +0900

    add_module comment 모듈 추가

--- init.js
+++ init.js
@@ -3,6 +3,7 @@ import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
 import "./models/Video";
+import "./models/Comment";
 
 const PORT = process.env.PORT;
 const handleListening = () =>  console.log(`Listening on : http://localhost:${PORT}`);
