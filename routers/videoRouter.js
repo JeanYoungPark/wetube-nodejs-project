646949f763a66b3e2310d2590b9b1b02c8719066
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-11-18 21:28:03 +0900

    editing video 기능 추가

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,16 +1,20 @@
 import express from "express";
 import routes from "../routes";
-import { deleteVideo, editVideo, postUpload, getUpload, video, videoDetail } from "../controller/videoController";
+import { deleteVideo, getEditVideo, postEditVideo, postUpload, getUpload, videoDetail } from "../controller/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+//upload
 videoRouter.get(routes.upload,getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
-videoRouter.get(routes.video,video);
+//video Detatil
 videoRouter.get(routes.videoDetail(),videoDetail);
-videoRouter.get(routes.editVideo,editVideo);
+
+//Edit Video
+videoRouter.get(routes.editVideo(),getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo)
 videoRouter.get(routes.deleteVideo,deleteVideo);
 
 export default videoRouter;
