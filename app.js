4355fb06ce15ad1c5e8143ffed5ae85a51bfc713
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-11-23 22:03:01 +0900

    setting babel/polyfill and scss

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-import express from "express";
-import morgan from "morgan";
-import helmet from "helmet";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
-import { localsMiddleware } from "./middlewares";
-import userRouter from "./routers/userRouter";
-import videoRouter from "./routers/videoRouter";
-import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import { localsMiddleware } from './middlewares';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
+import globalRouter from './routers/globalRouter';
+import routes from './routes';
 
 const app = express();
 
-app.set('view engine', "pug");
-app.use("/uploads", express.static("uploads"));
+app.set('view engine', 'pug');
+app.use('/uploads', express.static('uploads'));
+app.use('/static', express.static('static'));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(morgan("tiny"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
 app.use(helmet());
 app.use(localsMiddleware);
 
-app.use(routes.home,globalRouter);
-app.use(routes.users,userRouter);
-app.use(routes.video,videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.video, videoRouter);
 
 export default app;
