b99f3685408580c48b1a4a60065358304f6a2671
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-11-25 21:23:15 +0900

    passport이용하여 인증(2)

--- passport.js
+++ passport.js
@@ -2,3 +2,6 @@ import passport from 'passport';
 import User from './models/User';
 
 passport.use(User.createStategy());
+
+passport.serialiseUser(User.serialiseUser());
+passport.deserializeUser(User.deserializeUser());
