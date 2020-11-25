94a95160b027b528a2861201517b8971a797ff9e
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-11-25 20:08:40 +0900

    footer, Login and Join with scss

--- middlewares.js
+++ middlewares.js
@@ -1,17 +1,17 @@
-import multer from "multer";
-import routes from "./routes";
+import multer from 'multer';
+import routes from './routes';
 
-const multerVideo = multer({dest:"uploads/videos/"});
+const multerVideo = multer({ dest: 'uploads/videos/' });
 
-//미들웨어를 사용해 전역변수 생성
-export const localsMiddleware = (req,res,next) => {
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated : true,
-        id:1
-    }
-    next();
+// 미들웨어를 사용해 전역변수 생성
+export const localsMiddleware = (req, res, next) => {
+  res.locals.siteName = 'WeTube';
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1,
+  };
+  next();
 };
 
 export const uploadVideo = multerVideo.single('videoFile');
