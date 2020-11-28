f7ff36a4b93a80f1ebb59f0c14612fb4482eae99
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-11-28 17:48:33 +0900

    mongoStore and middlewares 세션저장 기능 구현

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import passport from 'passport';
+import mongoose from 'mongoose';
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import { localsMiddleware } from './middlewares';
 import routes from './routes';
 import userRouter from './routers/userRouter';
@@ -13,6 +15,9 @@ import globalRouter from './routers/globalRouter';
 import './passport';
 
 const app = express();
+
+const CokieStore = MongoStore(session);
+
 app.use(helmet());
 app.set('view engine', 'pug');
 app.use('/uploads', express.static('uploads'));
@@ -26,6 +31,7 @@ app.use(
     secret: process.env.COOKIE_SECRET,
     resave: true,
     saveUninitialized: false,
+    store: new CokieStore({ mongooseConnection: mongoose.connection }),
   }),
 );
 app.use(passport.initialize());
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,11 +3,12 @@ import {
   changePassword, editProfile, userDetail,
 } from '../controller/usersController';
 import routes from '../routes';
+import { onlyPrivate } from '../middlewares';
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
