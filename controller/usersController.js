9a2985c17ab886f06fd2dc71efc8d5d8990d2098
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-11-28 18:13:19 +0900

    github로 로그인하기(1)

--- controller/usersController.js
+++ controller/usersController.js
@@ -37,8 +37,14 @@ export const postLogin = passport.authenticate('local', {
   successRedirect: routes.home,
 });
 
+export const githubLoginGithub = (accessToken, refreshToken, profile, cb) => {
+  console.log(accessToken, refreshToken, profile, cb);
+};
+
+// export const githubLogin = passport.authenticate("github",)
+
 export const logout = (req, res) => {
-  // To Do: Process Log Out
+  req.logout();
   res.redirect(routes.home);
 };
 export const userDetail = (req, res) => res.render('userDetail', { pageTitle: 'User Detail' });
