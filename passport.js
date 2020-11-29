45ca04b6636995555c5f7df5a61e1afd7640e45d
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-11-29 17:45:13 +0900

    github login 기능구현(3)

--- passport.js
+++ passport.js
@@ -17,5 +17,12 @@ passport.use(
   ),
 );
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
