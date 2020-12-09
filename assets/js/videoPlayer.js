cb987fe16110f6d0797271e3dda1ee85bb90a81a
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-12-09 20:35:22 +0900

    play and pause js기능 추가

---
+++ assets/js/videoPlayer.js
@@ -0,0 +1,18 @@
+const videoContaier = document.getElementById('jsVideoPlayer');
+const videoPlayer = document.querySelector('#jsVideoPlayer video');
+const playBtn = document.getElementById('jsPlayButton');
+
+function handlePlayClick() {
+  if (videoPlayer.paused) {
+    videoPlayer.play();
+  } else {
+    videoPlayer.pause();
+  }
+}
+
+function init() {
+  playBtn.addEventListener('click', handlePlayClick);
+}
+if (videoContaier) {
+  init();
+}
