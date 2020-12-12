930955c40be640194f31893eaf99e9c78603cabf
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-12-12 16:56:16 +0900

    api adding a comment 기능 추가 (1)

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from '../routes';
 import {
+  postAddComment,
   postRegisterView,
 } from '../controller/videoController';
 
 const apiRouter = express.Router();
 
 apiRouter.post(routes.registerView, postRegisterView);
+apiRouter.post(routes.addComment, postAddComment);
 
 export default apiRouter;
