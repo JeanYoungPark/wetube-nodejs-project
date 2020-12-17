59ea180160e0348c499edcfc1170d7676cebfc5a
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2020-12-17 21:25:34 +0900

    Can't resolve './src' 문제 미해결

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,42 +1,44 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
-const ExtractCSS = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, 'assets', 'js', 'main.js');
 const OUTPUT_DIR = path.join(__dirname, 'static');
+
 const config = {
-  devtool: 'cheap-module-source-map',
-  entry: ['@babel/polyfill', ENTRY_FILE],
+  entry: ENTRY_FILE,
   mode: MODE,
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.(scss)$/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: MiniCssExtractPlugin.loader,
           },
-        ],
-      },
-      {
-        test: /\.(scss)$/,
-        use: ExtractCSS.extract([
           {
             loader: 'css-loader',
-          }, {
+          },
+          {
             loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins() {
-                  return [autoprefixer({ browsers: 'cover 99.5%' })];
-                },
+                plugins: [
+                  [
+                    'autoprefixer',
+                    {
+                      browsers: 'cover 99.5%',
+                    },
+                  ],
+                ],
               },
             },
-          }, {
+          },
+          {
             loader: 'sass-loader',
           },
-        ]),
+        ],
       },
     ],
   },
@@ -44,7 +46,13 @@ const config = {
     path: OUTPUT_DIR,
     filename: '[name].js',
   },
-  plugins: [new ExtractCSS('styles.css')],
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+    }),
+  ],
 };
 
 module.exports = config;
